83354da6aa9ac21020415f9adaa7165a938df9a1
Revinskaya Anastasiya <revinskaya0anastasiya@gmail.com>
2018-01-04 09:17:38 +0000

    без основного слайдера

---
+++ assets/js/style.js
@@ -0,0 +1,26 @@
+       //слайдер "карусель" листание влево
+       document.getElementById('slider-left').onclick = sliderLeft;
+       var left = 0;
+
+       function sliderLeft() {
+           var block__carousel = document.getElementById('block__carousel');
+           left = left - 34;
+           if (left < -90) {
+               left = 0;
+           }
+           block__carousel.style.left = left + '%';
+       }
+
+       //слайдер "карусель" листание вправо
+
+       document.getElementById('slider-right').onclick = sliderRight;
+       var left = 0;
+
+       function sliderRight() {
+           var block__carousel = document.getElementById('block__carousel');
+           left = -left + 34;
+           if (left > -90) {
+               left = 0;
+           }
+           block__carousel.style.left = left + '%';
+       }
