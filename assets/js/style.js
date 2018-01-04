2f9ae79cc8d6da72161e8fa603f7bb55e82bc0b0
Revinskaya Anastasiya <revinskaya0anastasiya@gmail.com>
2018-01-04 19:47:53 +0000

    не сделана основная галлерея, изменено внешнее оформление

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,12 +3,12 @@
        var left = 0;
 
        function sliderLeft() {
-           var block__carousel = document.getElementById('block__carousel');
-           left = left - 34;
-           if (left < -90) {
-               left = 0;
-           }
-           block__carousel.style.left = left + '%';
+              var block__carousel = document.getElementById('block__carousel');
+              left = left - 34;
+              if (left < -90) {
+                     left = 0;
+              }
+              block__carousel.style.left = left + '%';
        }
 
        //слайдер "карусель" листание вправо
@@ -17,10 +17,11 @@
        var left = 0;
 
        function sliderRight() {
-           var block__carousel = document.getElementById('block__carousel');
-           left = -left + 34;
-           if (left > -90) {
-               left = 0;
-           }
-           block__carousel.style.left = left + '%';
+              var block__carousel = document.getElementById('block__carousel');
+              left = -left + 34;
+              if (left > -90) {
+                     left = 0;
+              }
+              block__carousel.style.left = left + '%';
        }
+
